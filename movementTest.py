b17d41d784e12eddea6da01474ad62d44708a43a
Adrian Vecina <avecinat@gmail.com>
2024-03-02 18:35:46 +0000

    keyboard control

--- movementTest.py
+++ movementTest.py
@@ -9,14 +9,15 @@ while True:
     with Input(keynames='curses') as input_generator:
         for e in input_generator:
             print(repr(e))
+            let = repr(e)
 
-    # if keyboard.read_key() == 'w':
-    #     tbot.forward(1)
-        
-    #     time.sleep(0.1)
-    # elif keyboard.read_key() == 'a':
-    #     tbot.turn_right(1)
-    #     time.sleep(0.1)
-    # elif keyboard.read_key() == 'd':
-    #     tbot.turn_left(1)
-    #     time.sleep(0.1)
+            if let == 'w':
+                tbot.forward(1)
+                
+                time.sleep(0.1)
+            elif let == 'a':
+                tbot.turn_right(1)
+                time.sleep(0.1)
+            elif let == 'd':
+                tbot.turn_left(1)
+                time.sleep(0.1)
