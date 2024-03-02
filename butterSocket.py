ddbc9c9ec181d6090d86b9c91b7503b274030024
Adrian Vecina <avecinat@gmail.com>
2024-03-02 21:34:29 +0000

    socket errors

--- butterSocket.py
+++ butterSocket.py
@@ -1,27 +1,30 @@
 from websockets.sync.client import connect
 from butterMove import ButterMove
-
+import asyncio
 
 SOCKET = "ws://localhost:8765/"
 
+async def getmsg():
+    async with connect(SOCKET) as websocket:
+        return await websocket.recv()
+
 while True:
     bot = ButterMove()
-    with connect(SOCKET) as websocket:
-        msg = websocket.recv()
-        let = str(msg)
+    
+    let = getmsg
 
-        if 'FORWARD' in let:
-            bot.foward()
-        elif 'TURN_R' in let:
-            bot.turnRight()
-        elif 'TURN_L' in let:
-            bot.turnLeft()
-        elif 'BACKWARDS' in let:
-            bot.backward()
-        elif 'TILT_U' in let:
-            bot.tiltUp()
-        elif 'TILT_D' in let:
-            bot.tiltDown()
-        elif 'STOP' in let:
-            bot.stopAll()
+    if 'FORWARD' in let:
+        bot.foward()
+    elif 'TURN_R' in let:
+        bot.turnRight()
+    elif 'TURN_L' in let:
+        bot.turnLeft()
+    elif 'BACKWARDS' in let:
+        bot.backward()
+    elif 'TILT_U' in let:
+        bot.tiltUp()
+    elif 'TILT_D' in let:
+        bot.tiltDown()
+    elif 'STOP' in let:
+        bot.stopAll()
 
--- sockTest.py
+++ sockTest.py
@@ -4,12 +4,8 @@ import websockets
 # Define the message you want to send
 message_to_send = "TURN_R\n"
 
-async def send_message(websocket, path):
-    # When a client connects, send the message and close the connection
-    await websocket.send(message_to_send)
-    await websocket.close()
+async def send_message():
+    server = await websockets.serve(message_to_send, 'localhost', 8765)
+    await server.wait_closed()
 
-start_server = websockets.serve(send_message, "localhost", 8765)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.run(send_message())
