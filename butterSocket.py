749dd73c8a441a8217198dabcd9532a7eeb367f6
Adrian Vecina <avecinat@gmail.com>
2024-03-02 21:40:03 +0000

    async

--- butterSocket.py
+++ butterSocket.py
@@ -4,27 +4,25 @@ import asyncio
 
 SOCKET = "ws://localhost:8765/"
 
-async def getmsg():
-    async with connect(SOCKET) as websocket:
-        return await websocket.recv()
-
-while True:
+async def run():
     bot = ButterMove()
-    
-    let = getmsg()
+    async with connect(SOCKET) as websocket:
+        while True:
+            let = await websocket.recv()
 
-    if 'FORWARD' in let:
-        bot.foward()
-    elif 'TURN_R' in let:
-        bot.turnRight()
-    elif 'TURN_L' in let:
-        bot.turnLeft()
-    elif 'BACKWARDS' in let:
-        bot.backward()
-    elif 'TILT_U' in let:
-        bot.tiltUp()
-    elif 'TILT_D' in let:
-        bot.tiltDown()
-    elif 'STOP' in let:
-        bot.stopAll()
+            if 'FORWARD' in let:
+                bot.foward()
+            elif 'TURN_R' in let:
+                bot.turnRight()
+            elif 'TURN_L' in let:
+                bot.turnLeft()
+            elif 'BACKWARDS' in let:
+                bot.backward()
+            elif 'TILT_U' in let:
+                bot.tiltUp()
+            elif 'TILT_D' in let:
+                bot.tiltDown()
+            elif 'STOP' in let:
+                bot.stopAll()
 
+asyncio.get_event_loop().run_until_complete(run())
