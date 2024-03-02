1878664aa306851d29cac935ebe8d8e875e0c95b
Adrian Vecina <avecinat@gmail.com>
2024-03-02 19:04:56 +0000

    Fixed Lights

--- movementTest.py
+++ movementTest.py
@@ -16,21 +16,25 @@ while True:
 
             if 'w' in let:
                 tbot.forward(1)
+                tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_FRONT_RIGHT, 0, 255, 0)  
                 tbot.set_underlight(LIGHT_FRONT_LEFT, 0, 255, 0)  
                 time.sleep(0.1)
             elif 'd' in let:
                 tbot.turn_right(0.7)
-                tbot.clear_underlight(LIGHT_FRONT_LEFT)
+                tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_FRONT_RIGHT, 0, 255, 0)
+                tbot.set_underlight(LIGHT_MIDDLE_RIGHT, 0, 255, 0)
                 time.sleep(0.1)
             elif 'a' in let:
                 tbot.turn_left(0.7)
-                tbot.clear_underlight(LIGHT_FRONT_RIGHT)
+                tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_FRONT_LEFT, 0, 255, 0)
+                tbot.set_underlight(LIGHT_MIDDLE_LEFT, 0, 255, 0)
                 time.sleep(0.1)
             elif 's' in let:
                 tbot.backward(1)
+                tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_REAR_RIGHT, 255, 0, 0)  
                 tbot.set_underlight(LIGHT_REAR_LEFT, 255, 0, 0)  
                 time.sleep(0.1)
