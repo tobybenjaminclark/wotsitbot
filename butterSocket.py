232b44871c9294733bd54a42e4ffc45d1e67cb3b
Adrian Vecina <avecinat@gmail.com>
2024-03-02 23:24:50 +0000

    wow

--- butterSocket.py
+++ butterSocket.py
@@ -2,6 +2,10 @@ from gevent import monkey
 
 monkey.patch_all(thread=False, select=False)
 
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 from butterMove import ButterMove
 from requests import Session
 from signalr import Connection
