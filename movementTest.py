ca311bce4ee6d8542a19c001ce742360e45dbc20
Adrian Vecina <avecinat@gmail.com>
2024-03-02 19:13:38 +0000

    addedServoMoves

--- movementTest.py
+++ movementTest.py
@@ -5,6 +5,7 @@ from curtsies import Input
 
 tbot = Trilobot()
 
+ang = 0
 while True:
     with Input(keynames='curses') as input_generator:
 
@@ -21,13 +22,13 @@ while True:
                 tbot.set_underlight(LIGHT_FRONT_LEFT, 200, 0, 255)  
                 time.sleep(0.1)
             elif 'd' in let:
-                tbot.turn_right(0.7)
+                tbot.turn_right(0.8)
                 tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_FRONT_RIGHT, 0, 255, 0)
                 tbot.set_underlight(LIGHT_MIDDLE_RIGHT, 0, 255, 0)
                 time.sleep(0.1)
             elif 'a' in let:
-                tbot.turn_left(0.7)
+                tbot.turn_left(0.9)
                 tbot.clear_underlighting()
                 tbot.set_underlight(LIGHT_FRONT_LEFT, 0, 255, 0)
                 tbot.set_underlight(LIGHT_MIDDLE_LEFT, 0, 255, 0)
@@ -38,8 +39,19 @@ while True:
                 tbot.set_underlight(LIGHT_REAR_RIGHT, 255, 0, 0)  
                 tbot.set_underlight(LIGHT_REAR_LEFT, 255, 0, 0)  
                 time.sleep(0.1)
-
-
+            elif 'q' in let:
+                tbot.clear_underlighting()
+                if ang != 45: 
+                   ang += 1
+                   tbot.set_servo_angle(ang)
+                   time.sleep(0.01)
+            elif 'e' in let:
+                tbot.clear_underlighting()
+                if ang != 45: 
+                   ang += 1
+                   tbot.set_servo_angle(ang)
+                   time.sleep(0.01)
             elif 'x' in let:
                 tbot.coast()
+                tbot.clear_underlighting()
                 print("stop")
