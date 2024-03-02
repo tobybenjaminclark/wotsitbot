ced7a0fb7255bbc2a771b44536fb3a240bc55102
Adrian Vecina <avecinat@gmail.com>
2024-03-02 18:34:09 +0000

    whoopsie

--- movementTest.py
+++ movementTest.py
@@ -1,7 +1,6 @@
 import time
 from trilobot import *
 import math
-import keyboard
 from curtsies import Input
 
 tbot = Trilobot()
@@ -11,13 +10,13 @@ while True:
         for e in input_generator:
             print(repr(e))
 
-    if keyboard.read_key() == 'w':
-        tbot.forward(1)
+    # if keyboard.read_key() == 'w':
+    #     tbot.forward(1)
         
-        time.sleep(0.1)
-    elif keyboard.read_key() == 'a':
-        tbot.turn_right(1)
-        time.sleep(0.1)
-    elif keyboard.read_key() == 'd':
-        tbot.turn_left(1)
-        time.sleep(0.1)
+    #     time.sleep(0.1)
+    # elif keyboard.read_key() == 'a':
+    #     tbot.turn_right(1)
+    #     time.sleep(0.1)
+    # elif keyboard.read_key() == 'd':
+    #     tbot.turn_left(1)
+    #     time.sleep(0.1)
