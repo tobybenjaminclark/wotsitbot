8668a8e579a2643ad6c2a236268d68bd11c41f01
dthelegend <me@daudi.dev>
2024-03-03 00:03:37 +0000

    Refactor File system

---
+++ cheesecake/buttery_biscuit_base/butterSocket.py
@@ -0,0 +1,28 @@
+from websockets import connect
+from butterMove import ButterMove
+import asyncio
+
+SOCKET = "ws://192.168.243.226:8765/"
+
+async def run():
+    bot = ButterMove()
+    with connect(SOCKET) as websocket:
+        while True:
+            let = await websocket.recv()
+
+            if 'FORWARD' in let:
+                bot.foward()
+            elif 'TURN_R' in let:
+                bot.turnRight()
+            elif 'TURN_L' in let:
+                bot.turnLeft()
+            elif 'BACKWARDS' in let:
+                bot.backward()
+            elif 'TILT_U' in let:
+                bot.tiltUp()
+            elif 'TILT_D' in let:
+                bot.tiltDown()
+            elif 'STOP' in let:
+                bot.stopAll()
+
+asyncio.get_event_loop().run_until_complete(run())
