f7c258f6566c1d36dc8b70a7a06a006d40bda187
Adrian Vecina <avecinat@gmail.com>
2024-03-03 00:18:10 +0000

    please

--- butterSocket.py
+++ butterSocket.py
@@ -1,28 +1,31 @@
-from websockets import connect
 from butterMove import ButterMove
 import asyncio
+from websockets.server import serve
 
-SOCKET = "ws://192.168.243.226:8765/"
+SOCKET = "ws://0.0.0.0:8765/"
 
-async def run():
-    bot = ButterMove()
-    with connect(SOCKET) as websocket:
-        while True:
-            let = await websocket.recv()
+bot = ButterMove()
 
-            if 'FORWARD' in let:
-                bot.foward()
-            elif 'TURN_R' in let:
-                bot.turnRight()
-            elif 'TURN_L' in let:
-                bot.turnLeft()
-            elif 'BACKWARDS' in let:
-                bot.backward()
-            elif 'TILT_U' in let:
-                bot.tiltUp()
-            elif 'TILT_D' in let:
-                bot.tiltDown()
-            elif 'STOP' in let:
-                bot.stopAll()
+async def do(ws):
+    async for let in ws:
+        print(let)
+        if 'FORWARD' in let:
+            bot.foward()
+        elif 'TURN_R' in let:
+            bot.turnRight()
+        elif 'TURN_L' in let:
+            bot.turnLeft()
+        elif 'BACKWARDS' in let:
+            bot.backward()
+        elif 'TILT_U' in let:
+            bot.tiltUp()
+        elif 'TILT_D' in let:
+            bot.tiltDown()
+        elif 'STOP' in let:
+            bot.stopAll()
 
-asyncio.get_event_loop().run_until_complete(run())
+async def main():
+    async with serve(do, "0.0.0.0", 8765):
+        await asyncio.Future()
+    
+asyncio.run(main())
--- sockTest.py
+++ sockTest.py
@@ -5,7 +5,7 @@ import websockets
 message_to_send = "TURN_R\n"
 
 async def send_message():
-    server = await websockets.serve(message_to_send, '0.0.0.0', 8765)
+    server = await websockets.serve(message_to_send, '192.168.243.38', 8765)
     await server.wait_closed()
 
 asyncio.run(send_message())
