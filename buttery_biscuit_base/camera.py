0a7e91fc0f4443868d46c93e95419ee91b6f74fe
dthelegend <me@daudi.dev>
2024-03-03 03:36:56 +0000

    F

--- buttery_biscuit_base/camera.py
+++ buttery_biscuit_base/camera.py
@@ -1,12 +1,22 @@
 from picamera2 import Picamera2
-import cv2
+from picamera2.encoders import JpegEncoder
+import io
 
 class CameraFeed:
     def __init__(self):
         self.camera = Picamera2()
 
-        self.camera.configure(self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
-        self.camera.start()
+        video_config = self.camera.create_video_configuration(main={"size": (1920, 1080)})
+        self.camera.configure(video_config)
+
+        encoder = JpegEncoder(q=70)
+
+        self.bytes_io = io.BytesIO()
+
+        self.camera.start_recording(encoder, self.bytes_io)
 
     def get_current_image_bytes(self):
-        return cv2.imencode('.png', self.camera.capture_array())[1]
+        return self.bytes_io.read()
+
+    def __del__(self):
+        self.camera.stop_recording()
