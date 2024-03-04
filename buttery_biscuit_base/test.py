d6a15c4d9bbfc7d2dfa1a667725a3f0dcc7e9353
Adrian Vecina <avecinat@gmail.com>
2024-03-04 23:46:03 +0000

    the bell tolls.

--- buttery_biscuit_base/test.py
+++ buttery_biscuit_base/test.py
@@ -8,7 +8,7 @@ TEST_URI = "ws://192.168.243.38:8765"
 
 async def send_message():
     # Define the message you want to send
-    message_to_send = ""#"TURN_R\n"
+    message_to_send = "STOP\n"
     
     async with websockets.connect(TEST_URI) as ws:
         while True:
@@ -17,8 +17,7 @@ async def send_message():
 
             image = cv2.imdecode(np.asarray(bytearray(image), dtype='uint8'), cv2.IMREAD_COLOR)
             cv2.imshow("Hello", image)
-            cv2.waitKey(0)
-            zzz(0.5)
+            cv2.waitKey(1)
             
 
 if __name__=="__main__":
